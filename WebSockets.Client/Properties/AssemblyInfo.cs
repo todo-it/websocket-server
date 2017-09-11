6ca958932a6a0ef13ae7e517a584e0e5f9b1ac92
Dominik Pytlewski <dominik@pytlewski.eu>
2017-09-11 18:31:48 +0200

    Fixed assemblyinfo

--- WebSockets.Client/Properties/AssemblyInfo.cs
+++ WebSockets.Client/Properties/AssemblyInfo.cs
@@ -1,19 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebSockets.Client")]
+[assembly: AssemblyTitle("WebSockets.Client forked from ninjasource/websocket-server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WebSockets.Client")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebSockets.Client forked from ninjasource/websocket-server")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
